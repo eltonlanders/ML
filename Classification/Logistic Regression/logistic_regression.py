afd7934fd2b35a6b8362c4d18880596ee6d3debd
Elton <57378191+eltonlanders@users.noreply.github.com>
2020-09-08 12:16:57 +0530

    Update logistic_regression.py

--- Classification/Logistic Regression/logistic_regression.py
+++ Classification/Logistic Regression/logistic_regression.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values   #only age and salary chosen here
+X = dataset.iloc[:, [2, 3]].values  
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
@@ -29,8 +29,8 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix   #class contains capital in the name and functions do not
-cm = confusion_matrix(y_test, y_pred)   #in the cm, the first and the last values of the matrix are the correct predictions and other diagonal are the incorrect ones
+from sklearn.metrics import confusion_matrix  
+cm = confusion_matrix(y_test, y_pred)   
 print(cm)
 
 # Visualising the Training set results
@@ -59,7 +59,7 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),   #contour builds the prediction line
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),  
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
